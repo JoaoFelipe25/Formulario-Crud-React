cbe11a6f30e9ed431e1b0d984c19a03d31da0d21
JoaoFelipe25 <joaofelipemarcelinodacruz@yahoo.com.br>
2022-08-22 15:03:58 -0300

    formulariocrud

---
+++ src/components/UsersTable.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components'
+import styles from './styles.scss'
+
+//botão principal
+const Button = styled.button`
+    margin-left: 10px;
+    margin-right: 10px;
+    font-size: 18px;
+`
+
+const EditButton = styled(Button)`
+    background-color: lightyellow;
+`
+
+const RemoveButton = styled(Button)`
+    background-color: lightcoral;
+    
+`
+
+
+const UsersTable = (props) => (
+
+    
+
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Idade</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            {
+                props.users.length > 0 ?(
+                    props.users.map(
+                        (user) => (
+                            <tr key={user.id}>
+                                <td>{user.name}</td>
+                                <td>{user.username}</td>
+                                <td>{user.email}</td>
+                                <td>{user.age}</td>
+                                <td>
+                                    <EditButton
+                                        onClick={
+                                            () => props.editRow(user)
+                                        }
+                                    >
+                                        Editar
+                                    </EditButton>
+                                </td>
+                                <td>
+                                    <RemoveButton
+                                        onClick = {
+                                            () => props.deleteUser(user.id)
+                                        }
+                                    >
+                                        Excluir
+                                    </RemoveButton>
+                                </td>
+                            </tr>
+                        )
+                    )
+                ):( 
+                    <tr>
+                        <td colSpan={4}>Lista Vazia</td>
+                    </tr>
+                )
+            }
+        </tbody>
+    </table>
+)
+
+export default UsersTable
